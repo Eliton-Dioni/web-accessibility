2958d257108f331b079955fd099dde3306c62458
edioni <edioni@ps.grupopanvel.com.br>
2024-08-29 01:45:16 -0300

    feat: created blog with 100% accessibility

---
+++ src/pages/_app.tsx
@@ -0,0 +1,13 @@
+import { useEffect } from 'react';
+import '../styles/globals.css'
+import { axeAccessibilityReporter } from '../utils/axeAccessibilityReporter'
+
+function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    axeAccessibilityReporter();
+  }, []);
+
+  return <Component {...pageProps} />
+}
+
+export default MyApp
